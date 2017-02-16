6c58c534c4c5a6135b96b61c560acceda41595f7
Jpryce <Jpryce2@gmail.com>
2017-02-16 16:47:12 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -53,13 +53,50 @@ $('#img-3').click(function(){
     captionText.innerHTML = this.alt;
 });
 
+var modal = $('#myModal4');
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+	 img = $('img-4');
+	 modalImg = $("Tiffany-CT60_Email1.png");
+	 captionText = $("caption");
+
+$('#img-4').click(function(){
+    $('#myModal4').css("display", "block");
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+});
+
+var modal = $('#myModal5');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+	 img = $('img-5');
+	 modalImg = $("Tiffany-Sparkle-Email1.png");
+	 captionText = $("caption");
+
+$('#img-5').click(function(){
+    $('#myModal5').css("display", "block");
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+});
+
+var modal = $('#myModal6');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+	 img = $('img-6');
+	 modalImg = $("Tiffany-Bluebox-Email.png");
+	 captionText = $("caption");
+
+$('#img-6').click(function(){
+    $('#myModal6').css("display", "block");
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+});
 // Get the <span> element that closes the modal
-// var span = $(".close");
+var span = $(".close");
 
-// // When the user clicks on <span> (x), close the modal
-// $('.close').click(function() { 
-//   $('.modal.'),css("display", "none";
-// }
+// When the user clicks on <span> (x), close the modal
+$('.close').click(function() { 
+  $('.modal').css("display", "none");
+})
 
 });
